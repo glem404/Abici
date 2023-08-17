eb1018a6507aefe2173529a9a4d375e1b0b9f527
Laverde22 <laverdemc77@gmail.com>
2023-08-17 13:00:57 -0500

    Vista Index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,14 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/index.css')}}">
         <title>Abici</title>
     </head>
     <body >
-        <div class="Container">
-            
-        </div>
+        @include('navegacion.nav')
+    <div class="fotodomi">
+        <img src="" alt="Foto Domi">
+    </div>
+       
     </body>
 </html>
---
+++ resources/views/navegacion/nav.blade.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('css/navegacion.css')}}">
+
+    <title>nav</title>
+</head>
+@yield('nav')
+<body>
+    <div class="contenedor-nav">
+        <nav>
+            <div>
+               <p>ABICI</p>
+            </div>
+            <ul>
+                <a href="#">Inicio</a>
+                <a href="#">Agregar Domicilio</a>
+            </ul>
+        </nav>
+    </div>
+</body>
+</html>
